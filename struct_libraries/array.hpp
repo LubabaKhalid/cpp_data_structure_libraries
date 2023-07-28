13e785cdf4459e4460ea3409ca6b01407f1f6a4c
Lubaba Khalid <bsdsf22a025@pucit.edu.pk>
2023-07-28 20:41:09 +0500

    code of array.hpp

--- struct_libraries/array.hpp
+++ struct_libraries/array.hpp
@@ -51,49 +51,139 @@ void SetArray(Array arr, const int size, int arr1[])
 }
 void Resize(Array& arr, const int s)
 {
-    cout<<"\n\n------Resize Incomplete------\n\n";
+    Array newarr=Create(s);
+    int minsize;
+    if(s<arr.size)
+    {
+        minsize=s;
+    }
+    else
+    {
+        minsize=arr.size;
+    }
+    for(int i=0;i<minsize;i++)
+    {
+        newarr.arr[i]=arr.arr[i];
+    }
+    DeleteArray(arr);
+    arr.arr=newarr.arr;
+    arr.size=s;
     
 }
 Array Copy(const Array arr)
 {
-    cout<<"\n\n------Copy Incomplete------\n\n";
-    Array a;
-    return a;
+    
+    Array coparr=Create(arr.size);
+    for(int i=0;i<arr.size;i++)
+    {
+        coparr.arr[i]=arr.arr[i];
+    }
+    return coparr;
 }
 int Find(const Array arr, const int val)
 {
-    cout<<"\n\n------Find Incomplete------\n\n";
+    for(int i=0;i<arr.size;i++)
+    {
+        if(arr.arr[i]==val)
+        {
+            return i;
+        }
+    }
     return -2;
 }
 Array Concat(Array arr1, Array arr2)
 {
-    cout<<"\n\n------Concat Incomplete------\n\n";
-    Array a;
-    return a;
+    Array arr3=Create(arr1.size+arr2.size);
+    for(int i=0;i<arr1.size;i++)
+    {
+        arr3.arr[i]=arr1.arr[i];
+    }
+    for(int i=0;i<arr2.size;i++)
+    {
+        arr3.arr[i]=arr2.arr[i];
+    }
+    return arr3;
 }
 void Sort(Array arr)
 {
-    cout<<"\n\n------Sort Incomplete------\n\n";
+    for(int i=0;i<arr.size-1;i++)
+    {
+        if(arr.arr[i]>arr.arr[i+1])
+        {
+            arr.arr[i],arr.arr[i+1]=arr.arr[i+1],arr.arr[i];
+        }
+    }
 }
 Array Union(Array arr1, Array arr2)
 {
-    cout<<"\n\n------Union Incomplete------\n\n";
-    Array a;
-    return a;
+    Array uarr=Create();
+   for(int i=0;i<arr1.size-1;i++)
+    {
+        if(arr1.arr[i]>arr1.arr[i+1])
+        {
+            arr1.arr[i],arr1.arr[i+1]=arr1.arr[i+1],arr1.arr[i];
+        }
+    }
+    for(int i=0;i<arr2.size-1;i++)
+    {
+        if(arr2.arr[i]>arr2.arr[i+1])
+        {
+            arr2.arr[i],arr2.arr[i+1]=arr2.arr[i+1],arr2.arr[i];
+        }
+    }
+    int i=0,j=0,c=0;
+    while(i<arr1.size && j<arr2.size)
+    {
+        if(arr1.arr[i]==arr2.arr[j])
+        {
+            uarr.arr[c]=arr1.arr[i];
+            i++;
+            j++;
+            c++;
+        }
+        else if(arr1.arr[i]<arr2.arr[j])
+        {
+            uarr.arr[c]=arr1.arr[i];
+            i++;
+            c++;
+        }
+        else
+        {
+            uarr.arr[c]=arr2.arr[j];
+            j++;
+            c++;
+        }
+    }
+    
+    return uarr;
 }
 Array Intersection(Array arr1, Array arr2)
 {
-    cout<<"\n\n------Intersection Incomplete------\n\n";
-    Array a;
-    return a;
+    Array inarr=Create();
+    int c=0;
+    for(int i=0;i<arr1.size;i++)
+    {
+        for(int j=0;j<arr2.size;j++)
+        {
+            if(arr1.arr[i]==arr2.arr[j])
+            {
+                inarr.arr[c]=arr1.arr[i];
+                c++;
+            }
+        }
+    }
+    return inarr;
 }
 Array Difference(Array arr1, Array arr2)
 {
-    cout<<"\n\n------Difference Incomplete------\n\n";
-    Array a;
-    return a;
+    Array arr3=Create(arr1.size);
+    for(int i=0;i<arr1.size;i++)
+    {
+        arr3.arr[i]=arr1.arr[i]-arr2.arr[i];
+    }
+    return arr3;
 }
 void Push(Array& arr1, const int& val)
 {
-    cout<<"\n\n------Push Incomplete------\n\n";
+    
 }
